27281bfa73ab7a377e2bbdcd63518508f4cb8328
Harsh <30972219+h-sinha@users.noreply.github.com>
2021-02-19 22:47:03 +0530

    Create 1490D.cpp

---
+++ Codeforces/1490D.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define DEBUG
+#ifdef DEBUG
+#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+	template <typename Arg1>
+	void __f(const char* name, Arg1&& arg1){
+		cerr << name << " : " << arg1 << std::endl;
+	}
+	template <typename Arg1, typename... Args>
+	void __f(const char* names, Arg1&& arg1, Args&&... args){
+		const char* comma = strchr(names + 1, ','); cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+	}
+#else
+#define debug(...)
+#endif
+#define FOR(i,a,b) 	for(int i=a;i<b;++i)
+#define RFOR(i,a,b) 	for(int i=a;i>=b;--i)
+#define ln 		"\n"
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define sz(a)	ll(a.size())
+#define F first
+#define S second
+#define all(c)	c.begin(),c.end()
+#define rall(c) c.rbegin(), c.rend()
+#define trace(c,x) for(auto &x:c)
+#define pii pair<ll,ll>
+#define init(a, x) memset(a,x,sizeof(a))
+typedef long long ll;
+typedef long double ld;
+typedef	priority_queue<pii,std::vector<pii>,greater<pii> > revpr;
+
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+// ordered_set X
+//K-th smallest
+//*X.find_by_order(k-1)
+//NO OF ELEMENTS < A
+//X.order_of_key(A)
+const int L=1e2+7;
+map<ll,ll> counter;
+ll fastexpo(ll x,ll y,ll m=1e9+7)
+{
+	ll temp=1;
+	while(y>0)
+	{
+		if(y&1)temp = ((temp%m)*(x%m))%m;
+		x = ((x%m)*(x%m))%m;
+		y>>=1;
+	}return temp;
+}
+int a[L], p[L], ans[L];
+void solve(int l, int r, int cur, int d = 0)
+{
+	if(l > r)return;
+	if(l == r)
+	{
+		ans[a[l]] = d + 1;
+		return;
+	}
+	while(cur >= 1)
+	{
+		if(p[cur] >= l && p[cur] <= r)break;
+		cur--;
+	}
+	if(!cur)return;
+	ans[cur] = d + 1;
+	solve(l, p[cur] - 1, cur, d + 1);
+	solve(p[cur] + 1, r, cur, d + 1);
+}
+void fff()
+{
+	int n;
+	cin >> n;
+	FOR(i,0,n)cin >> a[i], p[a[i]] = i;
+	ans[n] = 0;
+	solve(0, p[n] - 1, n);
+	solve(p[n] + 1, n-1, n);
+	FOR(i,0,n)cout << ans[a[i]] << " ";
+	cout << ln;
+}
+int main()
+{
+	#ifdef LOCAL_EXEC
+		freopen("in.txt", "r", stdin);
+	 	freopen("out.txt", "w", stdout);
+	#else	
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+	int t;cin >> t;while(t--)
+	fff();
+	#endif
+	return 0;
+}
