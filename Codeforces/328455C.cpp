718adc6126fc2cece412940394c274f23c037fa4
Harsh <30972219+h-sinha@users.noreply.github.com>
2021-05-17 09:06:11 +0530

    Add files via upload

---
+++ Codeforces/328455C.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define DEBUG
+#ifdef DEBUG
+#define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+	template <typename Arg1>
+	void __f(const char* name, Arg1&& arg1){
+		cerr << name << " : " << arg1 << std::endl;
+	}
+	template <typename Arg1, typename... Args>
+	void __f(const char* names, Arg1&& arg1, Args&&... args){
+		const char* comma = strchr(names + 1, ','); cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+	}
+#else
+#define debug(...)
+#endif
+#define FOR(i,a,b) 	for(int i=a;i<b;++i)
+#define RFOR(i,a,b) 	for(int i=a;i>=b;--i)
+#define ln 		"\n"
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define sz(a)	ll(a.size())
+#define F first
+#define S second
+#define all(c)	c.begin(),c.end()
+#define rall(c) c.rbegin(), c.rend()
+#define trace(c,x) for(auto &x:c)
+#define pii pair<ll,ll>
+#define init(a, x) memset(a,x,sizeof(a))
+typedef long long ll;
+typedef long double ld;
+typedef	priority_queue<pii,std::vector<pii>,greater<pii> > revpr;
+
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> pbds;
+// ordered_set X
+//K-th smallest
+//*X.find_by_order(k-1)
+//NO OF ELEMENTS < A
+//X.order_of_key(A)
+const int L=1e6+7;
+map<ll,ll> counter;
+ll fastexpo(ll x,ll y,ll m=1e9+7)
+{
+	ll temp=1;
+	while(y>0)
+	{
+		if(y&1)temp = ((temp%m)*(x%m))%m;
+		x = ((x%m)*(x%m))%m;
+		y>>=1;
+	}return temp;
+}
+int t[L];
+bool comp(char x, char y)
+{
+	return t[x - 'A'] > t[y- 'A'];
+}
+void fff()
+{
+	int k;
+	cin >> k;
+	string s;
+	cin >> s;
+	set<char>SET;
+	trace(s, x)
+	{
+		SET.insert(x);
+	}
+	std::vector<char> v;
+	FOR(i,0,13)cin >> t[i];
+	FOR(i,0,13)
+	{
+		v.pb(char(i + 'A'));
+	}
+	sort(all(v), comp);
+	trace(v,x)
+	{
+		if(SET.find(x) == SET.end())
+		{
+			cout << x << ln;
+			return;
+		}
+	}
+}
+int main()
+{
+		freopen("scoreboard.in", "r", stdin);
+	int t;cin >> t;while(t--)
+	fff();
+	return 0;
+}
